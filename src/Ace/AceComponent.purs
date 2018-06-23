04394fa5de000c22c167004e1b0656b47b9dadf9
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-06-22 20:01:18 -0500

    Some work on handling changes from Ace

--- src/Ace/AceComponent.purs
+++ src/Ace/AceComponent.purs
@@ -1,4 +1,9 @@
-module Ace.AceComponent (AceQuery(..), AceOutput(..), aceComponent) where
+module Ace.AceComponent
+  ( AceQuery(..)
+  , AceOutput(..)
+  , AceChange
+  , aceComponent
+  ) where
 
 import Prelude
 
@@ -7,9 +12,13 @@ import Ace.EditSession as Session
 import Ace.Editor as Editor
 import Ace.Types (Editor)
 import Data.Const (Const)
+import Data.Generic.Rep
+import Data.Generic.Rep.Show (genericShow)
 import Data.Maybe (Maybe(..))
 import Effect.Aff (Aff)
+import Halogen (HalogenM)
 import Halogen as H
+import Halogen.HTML (HTML)
 import Halogen.HTML as HH
 import Halogen.HTML.Properties as HP
 
@@ -23,14 +32,32 @@ type AceState
 data AceQuery a
   = Initialize a
   | Finalize a
-  | ChangeText String a
-  | HandleChange (H.SubscribeStatus -> a)
+  | ApplyChange AceChange a
+  | OnChange AceChange (H.SubscribeStatus -> a)
 
 type AceInput
     = Unit
 
 data AceOutput
-  = TextChanged String
+  = Change AceChange
+
+derive instance genericAceOutput :: Generic AceOutput _
+
+instance showAceOutput :: Show AceOutput where
+  show = genericShow
+
+type AceChange
+  = { action :: String -- "insert" or "remove"
+    , start ::
+        { row :: Int
+        , column :: Int
+        }
+    , end ::
+        { row :: Int
+        , column :: Int
+        }
+    , lines :: Array String
+    }
 
 -- | The Ace component definition.
 aceComponent :: H.Component HH.HTML AceQuery AceInput AceOutput Aff
@@ -51,76 +78,75 @@ initialState _ =
 -- As we're embedding a 3rd party component we only need to create a placeholder
 -- div here and attach the ref property which will let us reference the element
 -- in eval.
-render :: AceState -> HH.HTML Void (AceQuery Unit)
+render :: AceState -> HTML Void (AceQuery Unit)
 render _ =
   HH.div [ HP.ref (H.RefLabel "ace"), HP.class_ (HH.ClassName "ace-editor") ] []
 
 -- The query algebra for the component handles the initialization of the Ace
 -- editor as well as responding to the `ChangeText` action that allows us to
 -- alter the editor's state.
-eval :: AceQuery ~> H.ComponentDSL AceState AceQuery AceOutput Aff
+eval :: AceQuery ~> HalogenM AceState AceQuery (Const Void) Void AceOutput Aff
 eval = case _ of
-  Initialize next ->
-    evalInitialize next
+  Initialize next -> do
+    evalInitialize
+    pure next
   Finalize next ->
-     evalFinalize next
-  ChangeText text next ->
-    evalChangeText text next
-  HandleChange reply ->
-    evalHandleChange reply
+    evalFinalize next
+  ApplyChange change next -> do
+    evalApplyChange change
+    pure next
+  OnChange change reply ->
+    reply <$> evalOnChange change
 
 evalInitialize
-  :: forall x.
-     x
-  -> H.HalogenM AceState AceQuery (Const Void) Void AceOutput Aff x
-evalInitialize next = do
+  :: HalogenM AceState AceQuery (Const Void) Void AceOutput Aff Unit
+evalInitialize = do
   H.getHTMLElementRef (H.RefLabel "ace") >>= case _ of
-    Nothing -> pure unit
+    Nothing ->
+      pure unit
     Just el' -> do
       editor <- H.liftEffect $ Ace.editNode el' Ace.ace
       session <- H.liftEffect $ Editor.getSession editor
       H.modify_ (_ { editor = Just editor })
-      H.subscribe $ H.eventSource_ (Session.onChange session) (H.request HandleChange)
-  pure next
+      H.subscribe $
+        H.eventSource
+          (Session.onChange session)
+          (\change -> Just (OnChange change identity))
 
 evalFinalize
   :: forall x.
      x
-  -> H.HalogenM AceState AceQuery (Const Void) Void AceOutput Aff x
+  -> HalogenM AceState AceQuery (Const Void) Void AceOutput Aff x
 evalFinalize next = do
   -- Release the reference to the editor and do any other cleanup that a
   -- real world component might need.
   H.modify_ (_ { editor = Nothing })
   pure next
 
-evalChangeText
-  :: forall x.
-     String
-  -> x
-  -> H.HalogenM AceState AceQuery (Const Void) Void AceOutput Aff x
-evalChangeText text next = do
+evalApplyChange
+  :: AceChange
+  -> HalogenM AceState AceQuery (Const Void) Void AceOutput Aff Unit
+evalApplyChange change = do
   maybeEditor <- H.gets _.editor
   case maybeEditor of
-    Nothing -> pure unit
+    Nothing ->
+      pure unit
     Just editor -> do
-      current <- H.liftEffect $ Editor.getValue editor
-      when (text /= current) do
-        void $ H.liftEffect $ Editor.setValue text Nothing editor
-  H.raise $ TextChanged text
-  pure next
+      -- TODO: Apply change to editor contents
 
-evalHandleChange
+      -- current <- H.liftEffect $ Editor.getValue editor
+      -- when (text /= current) do
+      --   void $ H.liftEffect $ Editor.setValue text Nothing editor
+
+      pure unit
+
+evalOnChange
   :: forall x.
-     (H.SubscribeStatus -> x)
-  -> H.HalogenM AceState AceQuery (Const Void) Void AceOutput Aff x
-evalHandleChange reply = do
-  maybeEditor <- H.gets _.editor
-  case maybeEditor of
-    Nothing -> pure unit
-    Just editor -> do
-      text <- H.liftEffect (Editor.getValue editor)
-      H.raise $ TextChanged text
-  pure (reply H.Listening)
+     AceChange
+  -> HalogenM AceState AceQuery (Const Void) Void AceOutput Aff H.SubscribeStatus
+evalOnChange change = do
+  H.raise (Change change)
+  pure H.Listening
 
 initializer :: Maybe (AceQuery Unit)
 initializer =
--- src/RootComponent.purs
+++ src/RootComponent.purs
@@ -2,14 +2,16 @@ module RootComponent where
 
 import Prelude
 
-import Ace.AceComponent (AceOutput(..), AceQuery(..), aceComponent)
+import Ace.AceComponent (AceOutput(..), AceQuery(..), AceChange, aceComponent)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
-import Effect.Class.Console (log)
+import Effect.Class.Console (log, logShow)
+import Halogen (Component, ComponentSlot)
 import Halogen as H
 import Halogen.Aff as HA
+import Halogen.HTML (HTML)
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.VDom.Driver (runUI)
@@ -21,7 +23,7 @@ type State = { text :: String }
 -- | The query algebra for the app.
 data Query a
   = ClearText a
-  | HandleAceUpdate String a
+  | HandleAceOutput AceOutput a
 
 -- | The slot address type for the Ace component.
 data AceSlot = AceSlot
@@ -29,44 +31,49 @@ derive instance eqAceSlot :: Eq AceSlot
 derive instance ordAceSlot :: Ord AceSlot
 
 -- | The main UI component definition.
-ui :: H.Component HH.HTML Query Unit Void Aff
+ui :: Component HTML Query Unit Void Aff
 ui =
   H.parentComponent
-    { initialState: const initialState
+    { initialState
     , render
     , eval
     , receiver: const Nothing
     }
+
+initialState :: Unit -> State
+initialState _ = { text: "" }
+
+render
+  :: State
+  -> HTML (ComponentSlot HTML AceQuery Aff AceSlot (Query Unit)) (Query Unit)
+render { text: text } =
+  HH.div_
+    [ HH.h1_
+        [ HH.text "ace editor" ]
+    , HH.div_
+        [ HH.p_
+            [ HH.button
+                [ HE.onClick (HE.input_ ClearText) ]
+                [ HH.text "Clear" ]
+            ]
+        ]
+    , HH.div_
+        [ HH.slot AceSlot aceComponent unit handleAceOuput ]
+    , HH.p_
+        [ HH.text ("Current text: " <> text) ]
+    ]
+
   where
+    handleAceOuput :: AceOutput -> Maybe (Query Unit)
+    handleAceOuput output =
+      Just (HandleAceOutput output unit)
 
-  initialState :: State
-  initialState = { text: "" }
 
-  render :: State -> H.ParentHTML Query AceQuery AceSlot Aff
-  render { text: text } =
-    HH.div_
-      [ HH.h1_
-          [ HH.text "ace editor" ]
-      , HH.div_
-          [ HH.p_
-              [ HH.button
-                  [ HE.onClick (HE.input_ ClearText) ]
-                  [ HH.text "Clear" ]
-              ]
-          ]
-      , HH.div_
-          [ HH.slot AceSlot aceComponent unit handleAceOuput ]
-      , HH.p_
-          [ HH.text ("Current text: " <> text) ]
-      ]
-
-  eval :: Query ~> H.ParentDSL State Query AceQuery AceSlot Void Aff
-  eval (ClearText next) = do
-    _ <- H.query AceSlot $ H.action (ChangeText "")
-    pure next
-  eval (HandleAceUpdate text next) = do
-    H.modify_ (_ { text = text })
-    pure next
-
-  handleAceOuput :: AceOutput -> Maybe (Query Unit)
-  handleAceOuput (TextChanged text) = Just $ H.action $ HandleAceUpdate text
+eval :: Query ~> H.ParentDSL State Query AceQuery AceSlot Void Aff
+eval (ClearText next) = do
+  -- _ <- H.query AceSlot $ H.action (ChangeText "")
+  pure next
+eval (HandleAceOutput output next) = do
+  -- H.modify_ (_ { text = text })
+  logShow output
+  pure next
