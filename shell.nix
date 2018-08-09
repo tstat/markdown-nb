cdb008c683e22282159fe637242a7fc01ff87a23
Travis Staton <hello@travisstaton.com>
2018-08-09 18:01:11 -0400

    fix nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,4 +1,16 @@
-with import <nixos-unstable> {};
+let
+  config = {
+    allowUnfree = true;
+  };
+  hostPkgs = import <nixpkgs> {};
+  pinnedVersion = hostPkgs.lib.importJSON ./nix/nixpkgs-version.json;
+  pinnedPkgs = hostPkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs-channels";
+    inherit (pinnedVersion) rev sha256;
+  };
+
+in with import pinnedPkgs { inherit config; };
 stdenv.mkDerivation rec {
   name = "env";
   env = buildEnv { name = name; paths = buildInputs; };
